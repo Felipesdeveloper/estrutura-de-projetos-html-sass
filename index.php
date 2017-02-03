6d847d019467f174b19bd58dd52eb4c32d7a2ea8
Felipesdeveloper <felipe.sdeveloper@gmail.com>
2017-02-02 22:59:43 -0200

    Estrutura Básica para projetos HTML com Sass
    Uma estrutura básica para melhor organização dos arquivos dentro do
    nosso projeto, e tambem para uma melhora na qualidade do momento do
    desenvolvimento e manutenção.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Estrutura Básica de Arquivos</title>
+		<link rel="stylesheet" href="assets/css/style.css">
+	</head>
+	<body>
+		<h1>Estrutura Básica de Arquivos</h1>
+	</body>
+</html>
